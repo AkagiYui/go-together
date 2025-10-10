380095de0ee43a177b885a63a350dce675440b9d
akagiyui <akagiyui@yeah.net>
2025-10-11 01:19:44 +0800

    feat(nottodo): cors middleware

--- nottodo/main.go
+++ nottodo/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/akagiyui/go-together/common/model"
 	"github.com/akagiyui/go-together/rest"
 )
@@ -13,8 +15,29 @@ func (r *TestRequest) Handle(ctx *rest.Context) {
 	println(r.Test)
 }
 
+func CORSMiddleware() rest.HandlerFunc {
+	return func(ctx *rest.Context) {
+		ctx.Response.Header("Access-Control-Allow-Origin", "*")
+		ctx.Response.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		ctx.Response.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		ctx.Response.Header("Access-Control-Allow-Credentials", "true")
+		ctx.Response.Header("Access-Control-Max-Age", "86400")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.Status(http.StatusNoContent)
+			ctx.Result(nil)
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
 func main() {
 	s := rest.NewServer()
+
+	s.UseFunc(CORSMiddleware())
 
 	s.GETFunc("/healthz", func(ctx *rest.Context) {
 		ctx.Set("test", "123\n")
